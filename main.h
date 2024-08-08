676527d50fcd4232dfd938b71abd2e963a0bc697
Pk-addey <pkaddey@gmail.com>
2024-08-08 11:24:37 +0000

    Modified printf.c file.

--- main.h
+++ main.h
@@ -1,3 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct format
+{
+    char *id;
+    int (*f)(va_list);
+} print_t;
+
 int _printf(const char *format, ...);
-int print_char(char c);
-int print_string(const char *s);
+int _putchar(char c);
+int print_char(va_list val);
+int print_string(va_list val);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(va_list args);
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_bin(va_list val);
+int print_unsigned(va_list args);
+int print_oct(va_list val);
+int print_hex(va_list val);
+
+#endif
